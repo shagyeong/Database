6599f0c79ee13bf94a70615de4d768c25bfaaac0
shagyeong <shagyeong21@gmail.com>
2022-07-24 15:53:53 +0900

    ds13 grpah update

--- datastructure/13graph/graph.h
+++ datastructure/13graph/graph.h
@@ -9,16 +9,42 @@ using namespace std;
 
 class graph{
     public:
-        graph(string namev, vertex* adj[]);
+        graph(string namev, vertex adjv[]);
+        graph(const graph& graphv);
 
         string getname(void);
         vertex* getadj(void);
 
         void setname(string namev);
-        void setadj(vertex* adjv[]);
+        void setadj(vertex adjv[]);
 
-        void iter(vertex* adjv[]); //정점 배열로 순회하기
+        void iter(vertex adjv[]); //정점 배열로 순회하기
     private:
         string name;
-        vertex* adj;
+        vertex* adj; //첫 번째 요소의 포인터
 };
+
+graph::graph(string namev, vertex adjv[]){
+    name = namev;
+    adj = &(adjv[0]);
+}
+graph::graph(const graph& graphv){
+    name = graphv.name;
+    adj = graphv.adj;
+}
+
+string graph::getname(void){
+    return name;
+}
+vertex* graph::getadj(void){
+    return adj;
+}
+
+void graph::setname(string namev){
+    name = namev;
+}
+void graph::setadj(vertex adjv[]){
+    adj = &(adjv[0]);
+}
+
+//void iter(vertex adjv[]); //정점 배열로 순회하기
--- datastructure/13graph/main.cpp
+++ datastructure/13graph/main.cpp
@@ -3,7 +3,48 @@ using namespace std;
 
 #include "vertex.h"
 #include "branch.h"
+#include "graph.h"
 
 int main(void){
-    
+    vertex j01("j01");
+    vertex j02("j02");
+    vertex j03("j03");
+    vertex j04("j04");
+    vertex j05("j05");
+    vertex j06("j06");
+    vertex j07("j07");
+    vertex j08("j08");
+    vertex j09("j09");
+    vertex j10("j10");
+    vertex j11("j11");
+    vertex j12("j12");
+
+    vertex adj01[] = {j02, j03};
+    vertex adj02[] = {j04, j05};
+    vertex adj03[] = {j06};
+    vertex adj04[] = {j07};
+    vertex adj05[] = {j08};
+    vertex adj06[] = {j09};
+    vertex adj07[] = {j10};
+    vertex adj08[] = {j10};
+    vertex adj09[] = {j11};
+    vertex adj10[] = {j11};
+    vertex adj11[] = {j12};
+
+    graph gr_j01("j01", adj01);
+    graph gr_j02("j02", adj02);
+    graph gr_j03("j03", adj03);
+    graph gr_j04("j04", adj04);
+    graph gr_j05("j05", adj05);
+    graph gr_j06("j06", adj06);
+    graph gr_j07("j07", adj07);
+    graph gr_j08("j08", adj08);
+    graph gr_j09("j09", adj09);
+    graph gr_j10("j10", adj10);
+    graph gr_j11("j11", adj11);
+
+    //2선 정차시 경로
+    vertex transit01[] = {j01, j02, j04, j07, j10, j11, j12};
+
+    return 0;
 }
