0caf1a65ddd40278348f4df3285180ab519f8e06
shagyeong <shagyeong21@gmail.com>
2022-08-02 21:50:18 +0900

    chapter00 update

---
+++ basic/chapter00/06-02.cpp
@@ -0,0 +1,17 @@
+/*
+#include<iostream>
+using namespace std;
+
+int main(void){
+
+에서 06-01.cpp의 이어적기로 다음과 같이 변함
+
+#include<iostream>
+using namespace std;
+
+int main(void){cout << "21 555" << endl;}
+*/
+#include<iostream>
+using namespace std;
+
+int main(void){
