19d192e7fd53ca03a7c05613b3cd849a3a5b4f29
shagyeong <shagyeong21@gmail.com>
2024-11-26 19:30:28 +0900

    rm

--- shagyeong/BE/src/main/java/me/sangjun/springbootdeveloper/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package me.sangjun.springbootdeveloper.repository;
-
-import me.sangjun.springbootdeveloper.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
